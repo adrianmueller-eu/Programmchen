9fbd6d1ae54cb451a09ecdbb013520c5f0c2ddac
Adrian Müller <dev@adrianmueller.eu>
2022-02-20 15:26:20 +0200

    utils/utils.py: Add matexp

--- utils/utils.py
+++ utils/utils.py
@@ -24,3 +24,28 @@ def is_symmetric(a, rtol=1e-05, atol=1e-08):
         return False
     a[np.isnan(a)] = 0
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
+
+
+def matexp(A0, eps=1e-5):
+    from math import factorial
+    from .utils import is_symmetric
+
+    # there is a faster method for symmetric matrices
+    if is_symmetric(A0):
+        eigval, eigvec = np.linalg.eig(A0)
+        return eigvec @ np.diag(np.exp(eigval)) @ eigvec.T
+
+    Asum = np.zeros(A0.shape, dtype=float)
+    Asum_prev = None
+
+    for i in range(0,10000):
+        if i == 0:
+            A = np.eye(A0.shape[0]) # A^0 = I
+        else:
+            A = np.array(factorial(i-1) * A @ A0 / factorial(i), dtype=float)
+        Asum_prev = Asum.copy()
+        Asum += A
+        if np.sum(np.abs(Asum - Asum_prev)) < eps:
+            break # return when converged
+
+    return Asum
