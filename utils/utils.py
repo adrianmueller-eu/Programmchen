06180a02bf9f266e16bc78d5acf318aac1c09714
Adrian Müller <dev@adrianmueller.eu>
2022-03-25 23:24:40 +0000

    utils/utils.py: logbins: Remove non-positive data before plotting

--- utils/utils.py
+++ utils/utils.py
@@ -12,6 +12,12 @@ def bins_sqrt(data):
 def logbins(data, start=None, stop=None, num=None, scale=2):
     if start is None:
         start = min(data)/scale
+    if start <= 0:
+        data = np.array(data)
+        data_pos = data[data > 0]
+        print("Warning: Data set contains non-positive numbers (%.2f%%). They will be excluded for the plot." % (100*(len(data_pos)/len(data))))
+        data = data_pos
+        start = min(data)/scale
     if stop is None:
         stop = max(data)*scale
     if num is None:
