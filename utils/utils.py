cf017b80312dfdad61d07b43e2dfef394fb6d936
Adrian Müller <dev@adrianmueller.eu>
2022-04-16 15:29:47 +0300

    utils/utils.py: Add deg, rad, series; fix bug in matexp

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 
 def moving_avg(x, w=3):
@@ -42,30 +43,46 @@ def is_complex(a):
     return np.iscomplex(a).any()
 #    return a.dtype == "complex128"
 
-def matexp(A0, eps=1e-5):
+def deg(x):
+    return x/np.pi*180
+
+def rad(x):
+    return x/180*np.pi
+
+def matexp(A0):
     from math import factorial
 
     # there is a faster method for symmetric matrices
-    if is_symmetric(A0):
+    if is_symmetric(A0): # doesn't work with hermitian matrices for some reason
         eigval, eigvec = np.linalg.eig(A0)
         return eigvec @ np.diag(np.exp(eigval)) @ eigvec.T
 
-    if is_complex(A0):
-        t=complex
-    else:
-        t=float
-
-    Asum = np.zeros(A0.shape, dtype=t)
+    Asum = np.zeros(A0.shape, dtype=A0.dtype)
     Asum_prev = None
 
     for i in range(0,10000):
         if i == 0:
             A = np.eye(A0.shape[0]) # A^0 = I
         else:
-            A = np.array(factorial(i-1) * A @ A0 / factorial(i), dtype=t)
+            A = np.array(factorial(i-1) * A @ A0 / factorial(i), dtype=A0.dtype)
         Asum_prev = Asum.copy()
         Asum += A
-        if np.sum(np.abs(Asum - Asum_prev)) < eps:
-            break # return when converged
+        if np.sum(np.abs(Asum - Asum_prev)) < sys.float_info.epsilon:
+            return Asum # return when converged
 
-    return Asum
+    raise ValueError("Convergence failed! (try scipy.linalg.expm instead)")
+
+# e.g. series(lambda n: 1/factorial(2*n)) + series(lambda n: 1/factorial(2*n + 1))
+def series(f, pr=False):
+    res = 0.0
+
+    for i in range(100000):
+        res_i = float(f(i))
+        res += res_i
+        if pr:
+            print(i, res, res_i)
+        if res_i < sys.float_info.epsilon:
+            return res
+        if res == np.inf:
+            break
+    raise ValueError("Series doesn't converge!")
