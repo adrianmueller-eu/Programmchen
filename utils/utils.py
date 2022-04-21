b41a16ea84efaa975d47f439febc29a63f7145ca
Adrian Müller <dev@adrianmueller.eu>
2022-04-21 19:01:19 +0300

    utils/utils.py: Add reversed_keys

--- utils/utils.py
+++ utils/utils.py
@@ -26,3 +26,6 @@ def logbins(data, start=None, stop=None, num=None, scale=2):
     if num is None:
         num = bins_sqrt(data)
     return 10**(np.linspace(np.log10(start),np.log10(stop),num))
+
+def reversed_keys(d):
+    return {k[::-1]:v for k,v in d.items()}
