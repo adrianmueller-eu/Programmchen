fc456fd7198b8791e13a596a92939494d9f37ccd
Adrian Müller <dev@adrianmueller.eu>
2022-09-01 22:05:32 -0700

    SAT/__main__.py: Update help message

--- SAT/__main__.py
+++ SAT/__main__.py
@@ -4,10 +4,21 @@ from . import solve, table, modelCnt, cnf
 def main():
 
     if len(argv) == 1 or "-h" in argv or "--help" in argv:
-        print("This is a simple sat solver. Give a formula")
-        print("\t%s" % "e.g. \"(a -> b) & (b <-> c) <-> (b & ~c)\"")
-        print("\t%s" % "e.g. \"r and not q or not t\"")
-        print("\t%s" % "e.g. \"(~a | b) & (a -> c) & (b -> ~c) & a\"")
+        print("A SAT solver.")
+        print("Usage: \033[1msat\033[0m [-h | -t | -tt | -c | --cnf] \033[4mformula\033[0m")
+        print("\t\033[1m-t\033[0m, --table         Show the whole truth table.")
+        print("\t\033[1m-tt\033[0m, --table-true   Show the rows of the truth table where \033[4mformula\033[0m is satiesfied")
+        print("\t\033[1m-c\033[0m, --count         Count the world in which \033[4mformula\033[0m is satisfied.")
+        print("\t\033[1m--cnf\033[0m               Output \033[4mformula\033[0m in CNF.")
+        print("\t--dpll              Use python dpll solver instead of z3-solver.")
+        print("")
+        print("Examples:")
+        print("\tsat \"a and b\"")
+        print("\tsat \"(~a | b) & (a -> c) & (b -> ~c) & a\"")
+        print("\tsat -t \"a <-> b and a -> ~b\"")
+        print("\tsat -tt \"(a -> b) & (b <-> c) <-> (b & ~c)\"")
+        print("\tsat -c \"a <- not b and not c and b\"")
+        print("\tsat --cnf \"r and not q or not t\"")
         exit()
 
     def arg(s):
@@ -20,7 +31,7 @@ def main():
     useDPLL = arg("--dpll")
     showTable = arg("-t") or arg("--table")
     showCnf = arg("--cnf")
-    showTableTrueOnly = arg("-tt") or arg("--table-true-only")
+    showTableTrueOnly = arg("-tt") or arg("--table-true")
     showModelCnt = arg("-c") or arg("--count")
 
     if len(argv) > 2:
